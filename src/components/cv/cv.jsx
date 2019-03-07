2aa502b7905ba670884d70f53ca003668b1c649a
RALVA Affärsutveckling AB <henrik@ralva.se>
2019-03-07 09:46:59 +0100

    printing doc

--- src/components/cv/cv.jsx
+++ src/components/cv/cv.jsx
@@ -1,5 +1,28 @@
 import React, { Component } from 'react';
-import './cv.css';
+import stylingsa from './cv.css';
+
+const stylie={
+    'printcontainer':{
+        'fontSize': '11pt',
+        'width':'595pt',
+        'minHeight':'842pt',
+        'backgroundColor': 'bisque',
+        'display': 'block',
+        'boxSizing':' borderBox',
+    },
+    'printcolleft':{
+        'display': ' inlineBlock',
+        'backgroundColor': 'blue',
+        'height': '100%',
+        'width': '30%'
+    },
+    'printcolright':{
+        'display': 'inlineBlock',
+        'backgroundColor': 'red',
+        'height':'100%',
+        'width':'70%'
+    }
+}
 
 
 export class CV extends Component {
@@ -7,11 +30,16 @@ export class CV extends Component {
 
 
     componentDidMount() {
+
+
+
+
         var content = document.getElementById("printcont");
         var pri = document.getElementById("ifmcontentstoprint").contentWindow;
         pri.document.open();
         console.log('the inner html');
-        console.log(content);
+        console.log(stylingsa)
+        console.log(content.innerHTML);
         pri.document.write(content.innerHTML);
         pri.document.close();
         pri.focus();
@@ -22,13 +50,17 @@ export class CV extends Component {
 
     toBePrinted() {
 
-        return (
-            <div id="printcont" className="print-container">
 
-                <div className="print-col-left">
+
+        return (
+            <div id="printcont" style={stylie.printcontainer}>
+                adssaasddsa
+                <div  style={stylie.printcolleft}>
+                    hejhej
                 </div>
 
-                <div className="print-col-right">
+                <div style={stylie.printcolright}>
+                svej svej
                 </div>
 
             </div>
@@ -39,8 +71,8 @@ export class CV extends Component {
     render() {
         return (
             <div className="print-wrapper">
-            <iframe id="ifmcontentstoprint" className="iframe-container"> {this.toBePrinted()}    </iframe>
-            {this.toBePrinted()} 
+            <iframe id="ifmcontentstoprint" className="iframe-container">   </iframe>
+                {this.toBePrinted()} 
             </div>
         )
     }
